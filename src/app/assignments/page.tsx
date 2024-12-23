181a1c313b394cace7bf01c1e2e2292aa63fe5e4
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-12-23 12:30:45 +0100

    fix layout of assignments

--- src/app/assignments/page.tsx
+++ src/app/assignments/page.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 export default function Page() {
   return (
-    <div>
+    <>
       <AssignmentDashboard />
-    </div>
+    </>
   );
 }
--- src/features/assignments/components/assignment-dashboard.tsx
+++ src/features/assignments/components/assignment-dashboard.tsx
@@ -6,15 +6,15 @@ import { Plus } from "lucide-react";
 export default function AssignmentDashboard() {
   const assignments = dbAssignments;
   return (
-    <div className="container mx-auto my-4  w-full">
-      <div className="flex justify-between ">
+    <div className="container mx-auto my-4 w-full">
+      <div className="flex justify-between mx-8 my-0">
         <p className="text-2xl font-bold pt-2 pl-3">Assignments</p>
         <button className="flex rounded-md bg-black text-gray-200 py-2 px-2 pr-4">
           <Plus />
           Add Assignments
         </button>
       </div>
-      <div className="rounded-md bg-white my-4 py-2 px-4 border-1">
+      <div className="rounded-md bg-white mx-8 my-2 py-2 px-4 border-1">
         {assignments.map((assignment) => (
           <Assignment key={assignment.id} assignment={assignment} />
         ))}
