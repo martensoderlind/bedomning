abed430c9899bc56f081bfd1dcaf409bd31f1f12
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-12-26 16:11:06 +0100

    page for the home route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-import StudentList from "@/features/students/components/student-list";
-import React from "react";
-
 export default function Page() {
-  return <StudentList />;
+  return <div>Home</div>;
 }
