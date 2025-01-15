f86251fc14fedbc14aeacebb5f7823fcaeff4597
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-01-15 19:00:57 +0100

    styling

--- src/features/assignments/components/course-dashboard.tsx
+++ src/features/assignments/components/course-dashboard.tsx
@@ -3,7 +3,7 @@ import CourseForm from "./course-form";
 export async function CourseDashboard() {
   return (
     <div className="mx-8 my-4">
-      <p className="text-2xl font-bold pt-2 pl-3">Add Course</p>
+      <p className="text-2xl font-bold pt-2">Add Course</p>
       <CourseForm />
     </div>
   );
